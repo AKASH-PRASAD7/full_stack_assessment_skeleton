93b03b2137451c5b3a6bb9675fea0083842d13d9
akash@useteleport.com <akash.csel016@bppimt.ac.in>
2024-09-06 17:30:14 +0530

    Db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { HomeModule } from './modules/home/home.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DatabaseModule, HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
